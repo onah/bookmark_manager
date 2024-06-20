2ef71cbc161c9623b6a372f07d8ac3c12f65cc25
HANO Hiroyuki <onahdwahan@gmail.com>
2024-06-21 06:18:40 +0900

    move main function to cli

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,14 @@
+use crate::bookmark::BookmarksAccessor;
+use crate::fileio::FileSystemAccessor;
+use crate::settings;
 use clap::{Parser, Subcommand};
+
+#[derive(Debug, Subcommand)]
+pub enum SubCommand {
+    Add { url: String },
+    List,
+    Execute { id: u32 },
+}
 
 #[derive(Parser)]
 pub struct Cli {
@@ -7,14 +17,54 @@ pub struct Cli {
 }
 
 impl Cli {
-    pub fn get_subcommand(&self) -> &SubCommand {
-        &self.subcommand
+    pub fn run(&self) {
+        match &self.subcommand {
+            SubCommand::Add { url } => {
+                add(url);
+            }
+            SubCommand::List => {
+                list();
+            }
+            SubCommand::Execute { id } => {
+                execute(*id);
+            }
+        }
     }
 }
 
-#[derive(Debug, Subcommand)]
-pub enum SubCommand {
-    Add { url: String },
-    List,
-    Execute { id: u32 },
+fn add(url: &str) {
+    let settings = settings::Settings::new(FileSystemAccessor);
+    let bookmark_accessor = BookmarksAccessor::new(settings.get_bookmark_file());
+    let mut bookmarks = bookmark_accessor.load(FileSystemAccessor);
+
+    bookmarks.push(url.to_string());
+    println!("Bookmarks: {:?}", bookmarks);
+    let accessor = FileSystemAccessor;
+    bookmark_accessor.save(accessor, &bookmarks);
+}
+
+fn list() {
+    let settings = settings::Settings::new(FileSystemAccessor);
+    let bookmark_accessor = BookmarksAccessor::new(settings.get_bookmark_file());
+    let bookmarks = bookmark_accessor.load(FileSystemAccessor);
+
+    println!("{}", bookmarks);
+}
+
+fn execute(id: u32) {
+    let settings = settings::Settings::new(FileSystemAccessor);
+    let bookmark_accessor = BookmarksAccessor::new(settings.get_bookmark_file());
+    let mut bookmarks = bookmark_accessor.load(FileSystemAccessor);
+
+    let url = bookmarks.search(id).unwrap();
+    //println!("Executing: {}", url);
+    let browswer = r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe";
+    let _output = std::process::Command::new(browswer)
+        .arg(url)
+        .output()
+        .expect("failed to execute process");
+
+    bookmarks.countup(id);
+    let accessor = FileSystemAccessor;
+    bookmark_accessor.save(accessor, &bookmarks);
 }
--- src/main.rs
+++ src/main.rs
@@ -3,60 +3,10 @@ mod cli;
 mod fileio;
 mod settings;
 
-use bookmark::BookmarksAccessor;
 use clap::Parser;
-use cli::{Cli, SubCommand};
-use core::str;
-use fileio::FileSystemAccessor;
+use cli::Cli;
 
 fn main() {
     let args = Cli::parse();
-    match args.get_subcommand() {
-        SubCommand::Add { url } => {
-            add(&url);
-        }
-        SubCommand::List => {
-            list();
-        }
-        SubCommand::Execute { id } => {
-            execute(*id);
-        }
-    }
-}
-
-fn add(url: &str) {
-    let settings = settings::Settings::new(FileSystemAccessor);
-    let bookmark_accessor = BookmarksAccessor::new(settings.get_bookmark_file());
-    let mut bookmarks = bookmark_accessor.load(FileSystemAccessor);
-
-    bookmarks.push(url.to_string());
-    println!("Bookmarks: {:?}", bookmarks);
-    let accessor = FileSystemAccessor;
-    bookmark_accessor.save(accessor, &bookmarks);
-}
-
-fn list() {
-    let settings = settings::Settings::new(FileSystemAccessor);
-    let bookmark_accessor = BookmarksAccessor::new(settings.get_bookmark_file());
-    let bookmarks = bookmark_accessor.load(FileSystemAccessor);
-
-    println!("{}", bookmarks);
-}
-
-fn execute(id: u32) {
-    let settings = settings::Settings::new(FileSystemAccessor);
-    let bookmark_accessor = BookmarksAccessor::new(settings.get_bookmark_file());
-    let mut bookmarks = bookmark_accessor.load(FileSystemAccessor);
-
-    let url = bookmarks.search(id).unwrap();
-    //println!("Executing: {}", url);
-    let browswer = r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe";
-    let _output = std::process::Command::new(browswer)
-        .arg(url)
-        .output()
-        .expect("failed to execute process");
-
-    bookmarks.countup(id);
-    let accessor = FileSystemAccessor;
-    bookmark_accessor.save(accessor, &bookmarks);
+    args.run();
 }
